16c8401f0e2e7bec21a98133c438b72fd78761cc
Sylvie Dagoret <sylvie.dagoret-campagne@ijclab.in2p3.fr>
2023-06-15 16:50:32 +0200

    correct bug un py script

---
+++ spectrasynthesis/prospector/prospector_fors2/tests/run_prospector_spectrophotom.sh
@@ -0,0 +1,16 @@
+#/bin/sh -x
+
+#conda activate prospector
+
+python fit_params_fors2.py --inputfile ../../../../QueryCatalogs/data/FORS2spectraGalexKidsPhotom.hdf5 --objid 214 --datamode spectrohotom \
+--optimize \
+--outfile=run_optimize
+
+python fit_params_fors2.py --inputfile ../../../../QueryCatalogs/data/FORS2spectraGalexKidsPhotom.hdf5 --objid 214 --datamode spectrophotom \
+--optimize --emcee \
+--outfile=run_optimize_emcee
+
+
+python fit_params_fors2.py --inputfile ../../../../QueryCatalogs/data/FORS2spectraGalexKidsPhotom.hdf5 --objid 214 --datamode spectrophotom \
+--optimize --dynesty \
+--outfile=run_optimize_dynesty
