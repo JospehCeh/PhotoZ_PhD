965823fcd90322ba7f678b400785833f1b7b959f
dagoret <dagoret@lal.in2p3.fr>
2023-06-21 00:10:38 +0200

    remove olds

---
+++ spectrasynthesis/prospector/prospector_fors2/tests/run_prospector_photom_args.sh
@@ -0,0 +1,23 @@
+#/bin/sh -x
+
+#conda activate prospector
+
+while getopts i: flag
+do
+    case "${flag}" in
+        i) id=${OPTARG};;
+    esac
+done
+
+python fit_params_fors2.py --inputfile ../../../../QueryCatalogs/data/FORS2spectraGalexKidsPhotom.hdf5 --objid $id --datamode photom \
+--optimize \
+--outfile=run_optimize
+
+python fit_params_fors2.py --inputfile ../../../../QueryCatalogs/data/FORS2spectraGalexKidsPhotom.hdf5 --objid $id --datamode photom \
+--optimize --emcee \
+--outfile=run_optimize_emcee
+
+
+python fit_params_fors2.py --inputfile ../../../../QueryCatalogs/data/FORS2spectraGalexKidsPhotom.hdf5 --objid $id --datamode photom \
+--optimize --dynesty \
+--outfile=run_optimize_dynesty
